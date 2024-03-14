61e825f403f5c3f54ed7216d42516aae5c8c474f
David <uo289792@uniovi.es>
2024-03-14 10:18:26 +0100

    SDI-2324-713-sesión8-ejercicio-complementario1

---
+++ musicstoreap/routes/authors.js
@@ -0,0 +1,53 @@
+module.exports = function(app) {
+
+    app.get("/authors", function(req, res) {
+        let authors = [{
+            "name": "Alex Turner",
+            "group": "Arctic Monkeys",
+            "role": "cantante"
+        }, {
+            "name": "Angus Young",
+            "group": "AC/DC",
+            "role": "guitarrista"
+        }, {
+            "name": "Adam Levine",
+            "group": "Maroon 5",
+            "role": "cantante"
+        }];
+
+        let response = {
+            seller: "Autores",
+            authors: authors
+        };
+        res.render("authors/authors.twig", response);
+    });
+
+    app.get('/authors/add', function (req, res) {
+        res.render("authors/add.twig");
+    });
+
+    app.post('/authors/add', function (req, res) {
+        let response = "";
+        if (req.body.name !== null && typeof(req.body.name) != "undefined" && req.body.name.trim() !== "") {
+            response = "Nombre: " + req.body.name + "<br>";
+        } else {
+            response += "Nombre no enviado en la petición" + "<br>";
+        }
+        if (req.body.group !== null && typeof(req.body.group) != "undefined" && req.body.group.trim() !== "") {
+            response += "Grupo: " + req.body.group + "<br>";
+        } else {
+            response += "Grupo no enviado en la petición" + "<br>";
+        }
+        if (req.body.role !== null && typeof(req.body.role) != "undefined" && req.body.role.trim() !== "") {
+            response += "Rol: " + req.body.role + "<br>";
+        } else {
+            response += "Role no enviado en la petición" + "<br>";
+        }
+        res.send(response);
+    });
+
+    app.get("/authors*", function(req, res) {
+        res.redirect("/authors");
+    });
+
+};
